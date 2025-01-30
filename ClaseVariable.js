787f83674facc583f877dd129a3500a521993e1f
TheLiightBringer <andresroot201@outlook.com>
2025-01-29 22:05:39 -0500

    clase de variables

---
+++ ClaseVariable.js
@@ -0,0 +1,15 @@
+var miNumero1 = 0;
+console.log(miNumero1);
+
+let miNumero2 = 10;
+console.log(miNumero2);
+
+//Reglas para nombrar variables
+// letras $ _  y posterior mente numeros
+
+let $miNumero3 = 10;    
+console.log($miNumero3);   
+let _miNumero4 = 20;
+console.log(_miNumero4);
+let miNumero5 = 30;
+console.log(miNumero5);
